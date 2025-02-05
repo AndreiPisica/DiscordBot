fa48c94b588a3e4b02bef3a31ec8aef60ea8a995
Andrei Pisica <pisica134@gmail.com>
2025-02-05 21:36:16 +0200

    enclose everything in a try catch

--- main.py
+++ main.py
@@ -20,35 +20,38 @@ async def on_ready():
 
 @client.event
 async def on_presence_update(before, after):
-  global active_games
-  user_id = after.id
-  general_channel = before.guild.system_channel
+  try:
+    global active_games
+    user_id = after.id
+    general_channel = before.guild.system_channel
 
-  if general_channel is None:
-    general_channel = before.guild.get_channel(223478400390660096)
+    if general_channel is None:
+      general_channel = before.guild.get_channel(223478400390660096)
 
-  #if before.status != after.status:
-    #if after.status == discord.Status.online:
-      #await general_channel.send(f'{after.name} is now online')
+    #if before.status != after.status:
+      #if after.status == discord.Status.online:
+        #await general_channel.send(f'{after.name} is now online')
 
-  # Handle Game Start
-  if before.activity != after.activity and after.activity is not None:
-    game_name = after.activity.name
+    # Handle Game Start
+    if before.activity != after.activity and after.activity is not None:
+      game_name = after.activity.name
 
-    # Prevent duplicate start messages
-    if active_games.get(user_id) != game_name:
-      active_games[user_id] = game_name  # Mark game as active
-      await general_channel.send(f'{after.name} is now playing {game_name}')
-      repository.start_game_session(user_id, after.name, game_name)
+      # Prevent duplicate start messages
+      if active_games.get(user_id) != game_name:
+        active_games[user_id] = game_name  # Mark game as active
+        await general_channel.send(f'{after.name} is now playing {game_name}')
+        repository.start_game_session(user_id, after.name, game_name)
 
-  # Handle Game Stop
-  elif before.activity is not None and after.activity is None:
-    game_name = before.activity.name
+    # Handle Game Stop
+    elif before.activity is not None and after.activity is None:
+      game_name = before.activity.name
 
-    # Ensure game was previously tracked
-    if active_games.get(user_id) == game_name:
-      del active_games[user_id]  # Remove from active sessions
-      repository.end_game_session(user_id, game_name)
+      # Ensure game was previously tracked
+      if active_games.get(user_id) == game_name:
+        del active_games[user_id]  # Remove from active sessions
+        repository.end_game_session(user_id, game_name)
+  except Exception as err:
+      print(f"Error {err}")
 
 
 async def wait_until(target_time):
